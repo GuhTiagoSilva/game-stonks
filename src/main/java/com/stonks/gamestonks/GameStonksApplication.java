87421274a7904cf92c2ec9a832038fd97e512006
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2022-09-05 19:45:37 -0300

    Initial Commit

---
+++ src/main/java/com/stonks/gamestonks/GameStonksApplication.java
@@ -0,0 +1,13 @@
+package com.stonks.gamestonks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStonksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStonksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stonks/gamestonks/GameStonksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stonks.gamestonks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStonksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

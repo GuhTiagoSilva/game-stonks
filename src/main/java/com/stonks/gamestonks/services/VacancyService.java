456b6d4c3f4cc7c8bb3063005d00eae31302f63b
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2022-09-27 21:26:23 -0300

    Set authenticated user in vacancy creation

--- src/main/java/com/stonks/gamestonks/services/VacancyService.java
+++ src/main/java/com/stonks/gamestonks/services/VacancyService.java
@@ -1,6 +1,7 @@
 package com.stonks.gamestonks.services;
 
 import com.stonks.gamestonks.dto.VacancyDto;
+import com.stonks.gamestonks.models.PlayerModel;
 import com.stonks.gamestonks.models.VacancyModel;
 import com.stonks.gamestonks.repositories.VacancyRepository;
 import org.springframework.stereotype.Service;
@@ -11,17 +12,22 @@ public class VacancyService {
 
     private final VacancyRepository vacancyRepository;
 
-    public VacancyService(VacancyRepository vacancyRepository) {
+    private final AuthService authService;
+
+    public VacancyService(VacancyRepository vacancyRepository, AuthService authService) {
         this.vacancyRepository = vacancyRepository;
+        this.authService = authService;
     }
 
     @Transactional
     public void createVacancy(VacancyDto vacancyDto) {
-        // Buscar usuario logado e setar na vaga
+
+        PlayerModel playerModel = authService.authenticated();
         VacancyModel model = new VacancyModel();
         model.setDescription(vacancyDto.getDescription());
         model.setEndDate(vacancyDto.getEndDate());
         model.setVacancyType(vacancyDto.getVacancyType());
+        model.setAuthor(playerModel);
         vacancyRepository.save(model);
     }
 

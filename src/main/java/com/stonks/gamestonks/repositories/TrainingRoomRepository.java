e3587ea44fcbb925ecc67e554f91672ab573e175
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2022-10-02 00:00:00 -0300

    Ajustando query

--- src/main/java/com/stonks/gamestonks/repositories/TrainingRoomRepository.java
+++ src/main/java/com/stonks/gamestonks/repositories/TrainingRoomRepository.java
@@ -12,25 +12,31 @@ import java.util.Optional;
 public interface TrainingRoomRepository extends JpaRepository<TrainingRoomModel, Long> {
 
     @Query(value = "select\n" +
-            "ttr.id as id, " +
+            "ttr.id as id,\n" +
             "ttr.name as trainingRoomName,\n" +
             "ttr.description as trainingRoomDescription,\n" +
-            "concat (tu.first_name , ' ', tu.last_name) as responsible,\n" +
+            "concat (tu.first_name ,\n" +
+            "' ',\n" +
+            "tu.last_name) as responsible,\n" +
             "tg.name as game,\n" +
             "tt.start_date as startDate,\n" +
             "tt.end_date as endDate\n" +
-            "from tb_training_room ttr\n" +
+            "from\n" +
+            "tb_training_room ttr\n" +
             "inner join tb_users tu\n" +
-            "on tu.id = ttr.user_id\n" +
+            "on\n" +
+            "tu.id = ttr.user_id\n" +
             "inner join tb_training_games ttg\n" +
-            "on ttg.training_id = ttr.id\n" +
+            "on\n" +
+            "ttg.training_id = ttr.id\n" +
             "inner join tb_games tg\n" +
-            "on tg.id = ttg.game_id\n" +
+            "on\n" +
+            "tg.id = ttg.game_id\n" +
             "inner join tb_training tt\n" +
-            "on tt.id = ttr.id\n" +
-            "inner join tb_week_days twd\n" +
-            "on twd.training_id = tt.id\n" +
-            "where ttr.is_available = true\n", nativeQuery = true)
+            "on\n" +
+            "tt.id = ttr.id\n" +
+            "where\n" +
+            "ttr.is_available = true\n", nativeQuery = true)
     List<TrainingRoomProjection> findTrainingRoom();
 
     @Query(value = "select\n" +
@@ -50,9 +56,7 @@ public interface TrainingRoomRepository extends JpaRepository<TrainingRoomModel,
             "on tg.id = ttg.game_id\n" +
             "inner join tb_training tt\n" +
             "on tt.id = ttr.id\n" +
-            "inner join tb_week_days twd\n" +
-            "on twd.training_id = tt.id\n" +
-            "where ttr.is_available = true and ttr.id = :id limit 1\n", nativeQuery = true)
+            "where ttr.is_available = true and ttr.id = :id\n", nativeQuery = true)
     Optional<TrainingRoomProjection> findTrainingRoomById(@Param("id") Long id);
 
 }
